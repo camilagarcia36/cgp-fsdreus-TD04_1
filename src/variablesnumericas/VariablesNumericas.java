1bc79b49157c4e752a5d625e42742ca562997faf
camilagarcia36 <garciiaperezz@gmail.com>
2023-03-07 12:47:25 +0100

    Subido ejercicio 1 acabado

---
+++ src/variablesnumericas/VariablesNumericas.java
@@ -0,0 +1,23 @@
+package variablesnumericas;
+
+//EJERCICIO 1 
+public class VariablesNumericas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 10; 
+		int b = 5; 
+		
+		System.out.println("Suma a+b : " + (a+b));
+		System.out.println("Resta a+b : " + (a-b));
+		System.out.println("Multiplicacion a * b : " + (a*b));
+		System.out.println("Division a/b : " + (a/b));
+		System.out.println("El modulo es : " + (a%b));
+		
+		
+		
+	
+	}
+
+}
